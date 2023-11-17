9bfd484842c1a74991bdf28e59c246907a8e79d9
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-17 10:33:39 -0600

    step and count are calculating date

--- src/App.js
+++ src/App.js
@@ -3,33 +3,29 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
   const [todaysDate, setTodaysDate] = useState(new Date());
   const renderedDate = todaysDate.toString().slice(0, 15);
   // console.log(todaysDate.getDate(), todaysDate.getDate() + 1);
   return (
     <div className="app">
-      <Step
-        step={step}
-        setStep={setStep}
-        todaysDate={todaysDate}
-        setTodaysDate={setTodaysDate}
-      />
+      <Step step={step} setStep={setStep} />
       <Count
         count={count}
         setCount={setCount}
         todaysDate={todaysDate}
         setTodaysDate={setTodaysDate}
+        step={step}
       />
       <p>
-        {count} days from today is {renderedDate}
+        {count * step} days from today is {renderedDate}
       </p>
     </div>
   );
 }
 
-function Step({ step, setStep, todaysDate, setTodaysDate }) {
+function Step({ step, setStep }) {
   return (
     <div className="container">
       <button onClick={() => setStep((s) => s - 1)}>-</button>
@@ -39,16 +35,16 @@ function Step({ step, setStep, todaysDate, setTodaysDate }) {
   );
 }
 
-function Count({ count, setCount, todaysDate, setTodaysDate }) {
-  function handleIncreaseCount(todaysDate, setTodaysDate) {
+function Count({ count, setCount, todaysDate, setTodaysDate, step }) {
+  function handleIncreaseCount(todaysDate, setTodaysDate, step) {
     setCount((c) => c + 1);
-    const incrDate = todaysDate.getDate() + 1;
+    const incrDate = todaysDate.getDate() + 1 * step;
     setTodaysDate(new Date(2023, 10, incrDate));
   }
 
   function handleDecreaseCount(todaysDate, setTodaysDate) {
     setCount((c) => c - 1);
-    const decrDate = todaysDate.getDate() - 1;
+    const decrDate = todaysDate.getDate() - 1 * step;
     setTodaysDate(new Date(2023, 10, decrDate));
   }
 
@@ -58,7 +54,9 @@ function Count({ count, setCount, todaysDate, setTodaysDate }) {
         -
       </button>
       <p>Count: {count}</p>
-      <button onClick={() => handleIncreaseCount(todaysDate, setTodaysDate)}>
+      <button
+        onClick={() => handleIncreaseCount(todaysDate, setTodaysDate, step)}
+      >
         +
       </button>
     </div>
